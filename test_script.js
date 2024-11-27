34bbdda43cfe801449196429f22068812014fdbf
Jerry Adriane Gonçalves <jerry.goncalves@luizalabs.com>
2024-11-27 12:38:37 -0300

    chore: fix test script

--- test_script.js
+++ test_script.js
@@ -3,6 +3,7 @@ import amqp from 'k6/x/amqp';  // Importa a extensão AMQP
 export default function () {
     // Configurações de conexão AMQP
     const host = 'localhost:5672';        // Endereço do RabbitMQ
+    const vhost = 'test-vhost';           // Vhost a ser utilizado
     const username = 'guest';             // Usuário padrão do RabbitMQ
     const password = 'guest';             // Senha padrão do RabbitMQ
     const exchange = 'test-exchange';     // Nome do exchange existente
@@ -10,7 +11,7 @@ export default function () {
     const message = `Mensagem do K6: ${__VU} - ${__ITER}`; // Mensagem com VU e Iteração
 
     // Publicar a mensagem no exchange
-    amqp.PublishToExchange(host, username, password, exchange, routingKey, message);
+    amqp.PublishToExchange(host, vhost, username, password, exchange, routingKey, message);
 
     console.log(`Mensagem enviada: ${message}`);
 }
