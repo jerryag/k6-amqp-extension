d625a6381cd749cf53106bc5a8eef653dd49fe2a
Jerry Adriane Gonçalves <jerry.goncalves@luizalabs.com>
2024-11-28 15:44:51 -0300

    chore: aux methods

--- amqp.go
+++ amqp.go
@@ -11,21 +11,15 @@ import (
 const version = "0.0.1"
 
 // Estrutura principal da extensão AMQP
-type AMQP struct {
-	Version string
-}
+type AMQP struct {}
 
 func init() {
-	a := AMQP {
-		Version: version
-	}
-
 	// Registrar o módulo personalizado no k6
-	modules.Register("k6/x/amqp", &a)
+	modules.Register("k6/x/amqp", new(AMQP))
 }
 
 // Conectar ao RabbitMQ e obter utilizando as informações e credenciais recebidas
-func (a *AMQP) Connect(host, vhost, username, password) {
+func (a *AMQP) Connect(host, vhost, username, password string) (*amqp.Connection, error) {
 	// Montar a URL de conexão AMQP com usuário e senha
 	amqpURL := fmt.Sprintf("amqp://%s:%s@%s/%s", username, password, host, vhost)
 	redactedURL := fmt.Sprintf("amqp://%s:*********@%s/%s", username, host, vhost)
@@ -33,61 +27,65 @@ func (a *AMQP) Connect(host, vhost, username, password) {
 	conn, err := amqp.Dial(amqpURL)
 	if err != nil {
 		fmt.Printf("Erro ao conectar ao RabbitMQ (%S): %v\n", err, redactedURL)
-		return nil, err
+		return conn, err
 	}
 
 	return conn, nil
 }
 
 // Encerrar a conexão 
-func (a *AMQP) Disconnect(conn) {
+func (a *AMQP) Disconnect(conn amqp.Connection) {
 	conn.Close()
 }
 
 // Abrir e obter um canal na conexão fornecida
-func (a *AMQP) OpenChannel(conn) {
+func (a *AMQP) OpenChannel(conn amqp.Connection) (*amqp.Channel, error) {
 	ch, err := conn.Channel()
 	if err != nil {
 		fmt.Printf("Erro ao abrir o canal: %v\n", err)
-		return nil, err
+		return ch, err
 	}
 
 	return ch, nil
 }
 
 // Fechar o canal
-func (a *AMQP) CloseChannel(ch) {
+func (a *AMQP) CloseChannel(ch amqp.Channel) {
 	ch.Close()
 }
 
-// Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão fornecida
-func (a *AMQP) PublishToExchange(host, vhost, username, password, exchange, routingKey, message string) {
-	conn, err := Connect(host, vhost, username, password)
+// Publica uma mensagem em um exchange específico do RabbitMQ usando as informações fornecidas
+func (a *AMQP) Publish(host, vhost, username, password, exchange, routingKey, message string) error {
+	conn, err := a.Connect(host, vhost, username, password)
 	if err != nil {
-		return
+		return err
 	}
 
-	PublishToExchange(conn, exchange, routingKey, message)
+	err = a.PublishUsingConn(*conn, exchange, routingKey, message)
 	
-	defer Disconnect;
+	defer a.Disconnect(*conn);
+
+	return err
 }
 
-// Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão fornecida
-func (a *AMQP) PublishToExchange(conn, exchange, routingKey, message string) {
-	ch, err := OpenChannel(conn)
+// Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão e informações fornecida
+func (a *AMQP) PublishUsingConn(conn amqp.Connection, exchange, routingKey, message string) error {
+	ch, err := a.OpenChannel(conn)
 	if err != nil {
-		return
+		return err
 	}
 
-	PublishToExchange(ch, exchange, routingKey, message)
+	err = a.PublishUsingChannel(*ch, exchange, routingKey, message)
 
-	defer CloseChannel(ch)
+	defer a.CloseChannel(*ch)
+
+	return err
 }
 
 // Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão fornecida
-func (a *AMQP) PublishToExchange(ch, exchange, routingKey, message string) {
+func (a *AMQP) PublishUsingChannel(ch amqp.Channel, exchange, routingKey, message string) error {
 	// Publicar a mensagem no exchange com a routing key especificada
-	err = ch.Publish(
+	return ch.Publish(
 		exchange,   // Nome do exchange
 		routingKey, // Routing key
 		false,      // Mandatory (setando para false, se não houver um binding para a rk, a mensagem será descartada silenciosamente)
@@ -98,10 +96,4 @@ func (a *AMQP) PublishToExchange(ch, exchange, routingKey, message string) {
 			Timestamp:   time.Now(),
 		},
 	)
-	if err != nil {
-		fmt.Printf("Erro ao publicar a mensagem: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Mensagem publicada com sucesso no exchange '%s' com routing key '%s'\n", exchange, routingKey)
 }
