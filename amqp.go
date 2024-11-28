6264205a9776ae44573a3b1fd249fb243b9c7136
Jerry Adriane Gonçalves <jerry.goncalves@luizalabs.com>
2024-11-28 11:33:42 -0300

    chore: aux methods

--- amqp.go
+++ amqp.go
@@ -8,39 +8,84 @@ import (
 	"go.k6.io/k6/js/modules"
 )
 
+const version = '0.0.1
+'
 // Estrutura principal da extensão AMQP
-type AMQP struct{
-	version  string
+type AMQP struct {
+	Version string,
 }
 
 func init() {
+	a := AMQP {
+		Version: version
+	}
+
 	// Registrar o módulo personalizado no k6
-	//oAmqp := AMQP{}
-	modules.Register("k6/x/amqp", new(AMQP))
+	modules.Register("k6/x/amqp", &a)
 }
 
-// PublishToExchange publica uma mensagem em um exchange específico do RabbitMQ usando autenticação
-func (a *AMQP) PublishToExchange(host, vhost, username, password, exchange, routingKey, message string) {
+// Conectar ao RabbitMQ e obter utilizando as informações e credenciais recebidas
+func (a *AMQP) Connect(host, vhost, username, password) {
 	// Montar a URL de conexão AMQP com usuário e senha
 	amqpURL := fmt.Sprintf("amqp://%s:%s@%s/%s", username, password, host, vhost)
 	redactedURL := fmt.Sprintf("amqp://%s:*********@%s/%s", username, host, vhost)
-
-	// Conectar ao RabbitMQ
+	
 	conn, err := amqp.Dial(amqpURL)
 	if err != nil {
 		fmt.Printf("Erro ao conectar ao RabbitMQ (%S): %v\n", err, redactedURL)
-		return
+		return nil, err
 	}
-	defer conn.Close()
 
-	// Abrir um canal
+	return conn, nil
+}
+
+// Encerrar a conexão 
+func (a *AMQP) Disconnect(conn) {
+	conn.Close()
+}
+
+// Abrir e obter um canal na conexão fornecida
+func (a *AMQP) OpenChannel(conn) {
 	ch, err := conn.Channel()
 	if err != nil {
 		fmt.Printf("Erro ao abrir o canal: %v\n", err)
+		return nil, err
+	}
+
+	return ch, nil
+}
+
+// Fechar o canal
+func (a *AMQP) CloseChannel(ch) {
+	ch.Close()
+}
+
+// Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão fornecida
+func (a *AMQP) PublishToExchange(host, vhost, username, password, exchange, routingKey, message string) {
+	conn, err := Connect(host, vhost, username, password)
+	if err != nil {
 		return
 	}
-	defer ch.Close()
 
+	PublishToExchange(conn, exchange, routingKey, message)
+	
+	defer Disconnect;
+}
+
+// Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão fornecida
+func (a *AMQP) PublishToExchange(conn, exchange, routingKey, message string) {
+	ch, err := OpenChannel(conn)
+	if err != nil {
+		return
+	}
+
+	PublishToExchange(ch, exchange, routingKey, message)
+
+	defer CloseChannel(ch)
+}
+
+// Publica uma mensagem em um exchange específico do RabbitMQ usando a conexão fornecida
+func (a *AMQP) PublishToExchange(ch, exchange, routingKey, message string) {
 	// Publicar a mensagem no exchange com a routing key especificada
 	err = ch.Publish(
 		exchange,   // Nome do exchange
